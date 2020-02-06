18ee850e42f111ac72b488caf090169056d5ae02
Alexandher <12073184+aexion-dev@users.noreply.github.com>
2020-02-06 14:57:08 -0600

    added / response

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => { res.send('it is working!') });
+
 //Sign In
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt) });
 
